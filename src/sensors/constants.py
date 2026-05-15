23e26eed9001ee6147783dfdde7a10bc8d4c4fc0
Dragomir J <75580472+dragomirj@users.noreply.github.com>
2026-05-15 22:04:00 +0200

    Update SENSOR_POLLER_MAX_ERRORS from 10 to 9

--- src/sensors/constants.py
+++ src/sensors/constants.py
@@ -7,5 +7,5 @@
 # *****************************************************************************
 POLLED_SENSOR_READ_TIMEOUT = 1.5  # Timeout for reading from a polled sensor (seconds)
 INTERRUPT_SENSOR_MAX_QUEUE_SIZE = 64  # Maximum number of unprocessed events
-SENSOR_POLLER_MAX_ERRORS = 10  # Maximum consecutive read errors before giving up
+SENSOR_POLLER_MAX_ERRORS = 9  # Maximum consecutive read errors before giving up
 SENSOR_POLLER_BACKOFF_CAP = 30.0  # Upper bound on exponential backoff between retries (seconds)
